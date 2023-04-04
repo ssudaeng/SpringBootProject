5031d6184a539663fa4ac1d7999e40b2e0301331
suvin <asb9259@gmail.com>
2023-04-04 19:27:05 +0900

    suvin_commit

--- src/main/java/com/test/test/SubinTest.java
+++
@@ -1,6 +0,0 @@
-package com.test.test;
-
-public class SubinTest {
-
-	//œX¶óœX¶ó
-}
---
+++ src/main/java/com/test/test/SuvinTest.java
@@ -0,0 +1,11 @@
+package com.test.test;
+
+public class SuvinTest {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Á¦¹ß;;;;");
+
+	}
+
+}
