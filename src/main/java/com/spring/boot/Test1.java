ad05d39ab6dd316df9e3512310ba23e8fd9fdcb3
수진 <suzlnl@naver.com>
2023-04-04 19:25:54 +0900

    fix Test1

--- src/main/java/com/spring/boot/Test1.java
+++ src/main/java/com/spring/boot/Test1.java
@@ -3,13 +3,13 @@ package com.spring.boot;
 public class Test1 {
 
 	public static void main(String[] args) {
+		
+		
 		System.out.println("hi~~~");
-<<<<<<< HEAD
 		System.out.println("수정수정");
 		System.out.println("우왕");
-=======
 		System.out.println("수정수e정");
->>>>>>> ad8d2058cd63c52ca6212a35955a139be94f3ef1
+		
 	}
 
 }
